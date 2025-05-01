4ec9485731246f25f644edbc0f0766f38284e179
Gustav <tyontholen@gmail.com>
2025-05-01 13:42:57 +0200

    fix: inputs beyond index caused the program to crash

--- main.py
+++ main.py
@@ -37,10 +37,9 @@ def wpm_test(stdscr):
         if key in ("KEY_BACKSPACE", "\b", "\x7f"):
             if len(current_text) > 0:
                 current_text.pop()
-        else:          
+        elif len(current_text) < len(target_text):          
             current_text.append(key)
 
- 
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
