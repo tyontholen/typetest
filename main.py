bab2685160af03df8baae75bec541ae960e480d5
Gustav <tyontholen@gmail.com>
2025-05-01 14:11:36 +0200

    feat: timer implemented and finish of test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import curses
 from curses import wrapper
+import time
 
 def start_screen(stdscr):
     stdscr.clear()
@@ -11,6 +12,7 @@ def start_screen(stdscr):
 
 def display_text(stdscr, target, current, wpm=0):
     stdscr.addstr(target)
+    stdscr.addstr(1, 0 ,f"Words per muinute: {wpm}")
 
     for i, char in enumerate(current):
         correct_char = target[i]
@@ -23,14 +25,26 @@ def display_text(stdscr, target, current, wpm=0):
 def wpm_test(stdscr):
     target_text = "Hello world this is some test text for this amazing app!"
     current_text = []
+    wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True)
 
     while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
 
         stdscr.clear()
-
+        display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()   
-        display_text(stdscr, target_text, current_text)
-        key = stdscr.getkey()
+
+        if "".join(current_text) == target_text:
+            stdscr.nodelay(False)
+            break
+        
+        try:
+            key = stdscr.getkey()
+        except:
+            continue      
 
         if key == "\x1b":
             break
@@ -47,7 +61,12 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
-    wpm_test(stdscr)
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(2, 0, "You completed the text! Press any key to continue...")
+        key = stdscr.getkey()
+        if key == "\x1b":
+            break
 
 
 wrapper(main)
